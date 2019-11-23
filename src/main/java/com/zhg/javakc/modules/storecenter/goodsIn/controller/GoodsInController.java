262c41b8515ba08012311751d030af9480698cac
yuanxuye1_global <yuanxuyeyxy@163.com>
2019-11-23 17:58:31 +0800

    第二次提交 #y

--- src/main/java/com/zhg/javakc/modules/storecenter/goodsIn/controller/GoodsInController.java
+++ src/main/java/com/zhg/javakc/modules/storecenter/goodsIn/controller/GoodsInController.java
@@ -1,4 +1,28 @@
 package com.zhg.javakc.modules.storecenter.goodsIn.controller;
 
-public class GoodsInController {
+import com.zhg.javakc.base.page.Page;
+import com.zhg.javakc.modules.storecenter.goodsIn.entity.GoodsInEntity;
+import com.zhg.javakc.modules.storecenter.goodsIn.service.GoodsInService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping("/goods")
+public class GoodsInController{
+    @Autowired
+    GoodsInService goodsInService;
+
+    @RequestMapping("queryGoods")
+    public ModelAndView queryGoods(GoodsInEntity goodsInEntity, HttpServletRequest request, HttpServletResponse response){
+        ModelAndView modelAndView=new ModelAndView("goods/list");
+        Page<GoodsInEntity> page=goodsInService.queryGoodds(goodsInEntity,new Page<GoodsInEntity>(request,response));
+        modelAndView.addObject("page",page);
+
+    return modelAndView;
+    }
 }
