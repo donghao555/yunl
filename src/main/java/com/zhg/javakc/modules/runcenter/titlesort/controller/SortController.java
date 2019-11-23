7281c84fb48d45af939e56290c93729c891fcc8d
javakc74_global <javakc74_global@javakc.com>
2019-11-23 13:08:13 +0800

    第二次提交 李云龙

---
+++ src/main/java/com/zhg/javakc/modules/runcenter/titlesort/controller/SortController.java
@@ -0,0 +1,4 @@
+package com.zhg.javakc.modules.runcenter.titlesort.controller;
+
+public class SortController {
+}
---
+++ src/main/java/com/zhg/javakc/modules/runcenter/titlesort/entity/SortEntity.java
@@ -0,0 +1,4 @@
+package com.zhg.javakc.modules.runcenter.titlesort.entity;
+
+public class SortEntity {
+}
